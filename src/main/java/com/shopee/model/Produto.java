5a55fc3b69766b1bf1e66ba1d0e1ed4d2e2841b9
adrien <adrienbzr@outlook.com>
2026-03-22 17:11:08 -0300

    feat: implementando classe Produto.java em model

--- src/main/java/com/shopee/model/Produto.java
+++ src/main/java/com/shopee/model/Produto.java
@@ -1,4 +1,100 @@
 package com.shopee.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Produto {
+    private int id;
+    private String nome;
+    private BigDecimal preco; // Todos os valores em dinheiro serão tratados em bigdecimal (boa prática)
+    private String descricao;
+    private String categoria;
+    private int quantidadeEstoque;
+    private String imagemUrl;
+    private LocalDateTime dataCadastro;
+    private boolean ativo;
+    private int vendedorId;
+
+    public Produto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public void setImagemUrl(String imagemUrl) {
+        this.imagemUrl = imagemUrl;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public int getVendedorId() {
+        return vendedorId;
+    }
+
+    public void setVendedorId(int vendedorId) {
+        this.vendedorId = vendedorId;
+    }
 }
