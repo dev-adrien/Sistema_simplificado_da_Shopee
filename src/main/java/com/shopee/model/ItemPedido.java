a6cfa0dc548ea120f174e2fd43c51346d15d72cd
adrien <adrienbzr@outlook.com>
2026-03-22 17:35:48 -0300

    feat: implementação da classe ItemPedido, finalização das entidades principais

--- src/main/java/com/shopee/model/ItemPedido.java
+++ src/main/java/com/shopee/model/ItemPedido.java
@@ -1,4 +1,54 @@
 package com.shopee.model;
 
+import java.math.BigDecimal;
+
 public class ItemPedido {
+    private int id;
+    private int pedidoId;
+    private int produtoId;
+    private int quantidade;
+    private BigDecimal precoUnitario;
+
+    public ItemPedido() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(int pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public int getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(int produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
 }
