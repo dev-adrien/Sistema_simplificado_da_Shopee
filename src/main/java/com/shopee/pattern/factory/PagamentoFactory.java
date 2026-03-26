759530ec8fc1500175ca7e2563793ece4a220d16
adrien <adrienbzr@outlook.com>
2026-03-26 15:00:50 -0300

    feat: adiciona um método que cria estratégia de pagamento de acordo com o usuário

--- src/main/java/com/shopee/pattern/factory/PagamentoFactory.java
+++ src/main/java/com/shopee/pattern/factory/PagamentoFactory.java
@@ -1,4 +1,24 @@
 package com.shopee.pattern.factory;
 
+import com.shopee.pattern.strategy.BoletoStrategy;
+import com.shopee.pattern.strategy.CartaoCreditoStrategy;
+import com.shopee.pattern.strategy.PagamentoStrategy;
+import com.shopee.pattern.strategy.PixStrategy;
+
 public class PagamentoFactory {
+
+    // metodo estático para criar estratégia de pagamento de acordo com o que o cliente seleciona
+    public static PagamentoStrategy criarStrategy(int opcaoSelecionada) {
+        switch (opcaoSelecionada) {
+            case 1:
+                return new PixStrategy();
+            case 2:
+                return new BoletoStrategy();
+            case 3:
+                return new CartaoCreditoStrategy();
+            default:
+                // excessão caso inválido
+                throw new IllegalArgumentException("Opção de pagamento inválida! Escolha 1, 2 ou 3.");
+        }
+    }
 }
