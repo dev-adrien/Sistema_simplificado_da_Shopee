963048ae37560aecb2cf415146dbd81097321d1f
adrien <adrienbzr@outlook.com>
2026-03-26 15:10:32 -0300

    feat: adiciona um método que cria o usuário com base no tipo (vendedor ou cliente)

--- src/main/java/com/shopee/pattern/factory/UsuarioFactory.java
+++ src/main/java/com/shopee/pattern/factory/UsuarioFactory.java
@@ -1,4 +1,24 @@
 package com.shopee.pattern.factory;
 
+import com.shopee.model.Cliente;
+import com.shopee.model.Usuario;
+import com.shopee.model.Vendedor;
+
 public class UsuarioFactory {
+
+    // metodo estático para criar usuário para vendedor e cliente
+    public static Usuario criarUsuario(String tipo) {
+
+        if (tipo == null) {
+            throw new IllegalArgumentException("O tipo de usuário não pode ser nulo.");
+        }
+
+        if (tipo.toLowerCase().equals("cliente")) {
+            return new Cliente();
+        } else if (tipo.toLowerCase().equals("vendedor")) {
+            return new Vendedor();
+        } else {
+            throw new IllegalArgumentException("Tipo de usuário inválido: " + tipo + ".\nEscolha 'cliente' ou 'vendedor'.\n");
+        }
+    }
 }
