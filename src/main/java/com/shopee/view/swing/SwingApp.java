612c9b554e94efc8ffaef9d6fde8f263104dc3ce
adrien <adrienbzr@outlook.com>
2026-03-26 20:43:33 -0300

    feat: adiciona método main em SwingApp para execução do Swing

--- src/main/java/com/shopee/view/swing/SwingApp.java
+++ src/main/java/com/shopee/view/swing/SwingApp.java
@@ -1,4 +1,13 @@
 package com.shopee.view.swing;
 
+import javax.swing.SwingUtilities;
+
 public class SwingApp {
+    public static void main(String[] args) {
+        // inicialização na thread de eventos do Swing (boa prática)
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame telaLogin = new LoginFrame();
+            telaLogin.setVisible(true);
+        });
+    }
 }
